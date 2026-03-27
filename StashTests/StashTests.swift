45f646ff5480ff2ab64423b81b069145e103b1d2
Keenan Meyer <keenanmeyer25@gmail.com>
2026-03-26 21:58:46 -0400

    Initial Commit

---
+++ StashTests/StashTests.swift
@@ -0,0 +1,19 @@
+//
+//  StashTests.swift
+//  StashTests
+//
+//  Created by Keenan Meyer on 3/26/26.
+//
+
+import Testing
+@testable import Stash
+
+struct StashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
